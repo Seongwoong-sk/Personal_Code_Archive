61697d0a5bd85bc6d7d6eff0e362e27b713982bc
Seong Woong Kim <rlatjddnd3@gmail.com>
2022-03-28 16:24:47 +0900

    Create unzip_colab.py

---
+++ unzip_colab.py
@@ -0,0 +1,11 @@
+# 드라이브 마운트
+from google.colab import drive
+drive.mount('/content/drive')
+
+# 경로 이동
+%cd /content/drive/MyDrive/------
+!unzip -qq "zip 파일 경로"
+
+# 업로드가 잘 됐는지 파일 개수로 확인
+filepaths = list(glob('content/image/*.jpg'))
+len(filepaths)
