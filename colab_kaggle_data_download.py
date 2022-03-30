83d95141654cdeac989f2e6ac44416f3b0211a84
Seong Woong Kim <rlatjddnd3@gmail.com>
2022-03-30 10:31:47 +0900

    Update colab_kaggle_data_download.py

--- colab_kaggle_data_download.py
+++ colab_kaggle_data_download.py
@@ -1,6 +1,7 @@
 # 코랩에서 캐글 데이터 다운로드
 
 # kaggle.json 다운로드
+# 403 에러 발생 시 캐글 해당 대회 Rule Acception 확인 
 
 # kaggle.json 위치 변경 및 실행
 !mkdir -p ~/.kaggle
@@ -8,3 +9,4 @@
 !chmod 600 ~/.kaggle/kaggle.json
 
 !kaggle competitions download -c 대회명
+
