12e5e5126ba454d55a743b461fceaacb4468b1bb
Seong Woong Kim <rlatjddnd3@gmail.com>
2022-04-08 09:13:03 +0900

    Update Define Train\Validation Image Augmentations(Library).py

--- Define Train\Validation Image Augmentations(Library).py
+++ Define Train\Validation Image Augmentations(Library).py
@@ -36,3 +36,6 @@ def get_valid_transforms():
             Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225], max_pixel_value=255.0, p=1.0),
             ToTensorV2(p=1.0),
         ], p=1.)
+
+
+ds = Dataset(valid_, data_root, transforms=get_valid_transforms(), output_label=True)
