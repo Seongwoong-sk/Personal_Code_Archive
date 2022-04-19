8ba0c9373b5fb1726bd663525aaebf8064712276
Seong Woong Kim <rlatjddnd3@gmail.com>
2022-04-19 14:22:15 +0900

    Create download tar.gz.py

---
+++ download tar.gz.py
@@ -0,0 +1,6 @@
+!wget https://www.robots.ox.ac.uk/~vgg/data/pets/data/images.tar.gz
+  
+# /content/data 디렉토리를 만들고 해당 디렉토리에 다운로드 받은 압축 파일 풀기.
+!mkdir /content/data
+!tar -xvf images.tar.gz -C /content/data
+!tar -xvf annotations.tar.gz -C /content/data
