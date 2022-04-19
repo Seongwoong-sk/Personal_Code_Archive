2309f113dead98966a4fde56051c7ad1efe4b433
Seong Woong Kim <rlatjddnd3@gmail.com>
2022-04-19 16:43:42 +0900

    Update Training & Inference.py

--- MMDetection/Training & Inference.py
+++ MMDetection/Training & Inference.py
@@ -1,3 +1,4 @@
+################################################################################################################
 ### TRINING ###
 ################################################################################################################
 from mmdet.datasets import build_dataset # Dataset을 비로소 만듦
@@ -16,6 +17,7 @@ from mmdet.apis import train_detector
 datasets = [build_dataset(cfg.data.train)]
 
 
+# COCO dataset으로 weight가 학습된 model 생성 (공모델) build_detector (Sturcture?...)
 model = build_detector(cfg.model, train_cfg=cfg.get('train_cfg'), test_cfg=cfg.get('test_cfg'))
 model.CLASSES = datasets[0].CLASSES
 
@@ -31,6 +33,7 @@ mmcv.mkdir_or_exist(osp.abspath(cfg.work_dir)) # tutorials/
 train_detector(model, datasets, cfg, distributed=False, validate=True)
 
 
+################################################################################################################
 ### INFERENCE ###
 ################################################################################################################
 from mmdet.apis import inference_detector, init_detector, show_result_pyplot
@@ -42,5 +45,132 @@ img = cv2.imread('/content/kitti_tiny/training/image_2/000068.jpeg') # BGR
 model.cfg = cfg
 
 result = inference_detector(model, img) # Inference하면서 BGR을 RGB로 만듬
-show_result_pyplot(model, img, result)
+show_result_pyplot(model, img, result, score_thr=0.3)
+
+'''
+아래는 오류를 발생시킵니다. 
+현재 Customized 학습된 모델의 inference시 <image file로 인자가 주어졌을때 inference 오류 발생.> 
+img_path = '/content/data/images/Abyssinian_88.jpg'
+
+model.cfg = cfg
+
+result = inference_detector(model, img_path)
+show_result_pyplot(model, img, result, score_thr=0.3)
+'''
+
+
+################################################################################################################
+### Checkpoint 저장된 model 파일을 로딩하고 이를 이용하여 Inference 수행 ###
+################################################################################################################
+from mmdet.apis import show_result_pyplot
+
+checkpoint_file = '/mydrive/pet_work_dir/epoch_5.pth'
+
+# checkpoint 저장된 model 파일을 이용하여 모델을 생성, 이때 Config는 위에서 update된 config 사용. 
+model_ckpt = init_detector(cfg, checkpoint_file, device='cuda:0')
+
+# BGR Image 사용 
+img = cv2.imread('/content/data/images/Abyssinian_88.jpg')
+
+#model_ckpt.cfg = cfg
+
+result = inference_detector(model_ckpt, img)
+show_result_pyplot(model_ckpt, img, result, score_thr=0.3)
+
+
+################################################################################################################
+### 여러 개의 image들을 Inference 수행 ###
+################################################################################################################
+
+'''
+inference_detector(model, imgs) 에서 인자 imgs는 단일 이미지일 경우 string/array, 여러개의 이미지일 경우 list(string/array)를 입력
+show_result_pyplot(model_ckpt, img, result, score_thr=0.3)는 여러 개의 이미지를 한번에 나타내기 어려우므로 별도의 시각화 함수 get_detected_img()를 이용
+'''
+PET_CLASSES = pet_df['class_name'].unique().tolist()
+labels_to_names_seq = {i:k for i, k in enumerate(PET_CLASSES)}
+
+# model과 원본 이미지 array, filtering할 기준 class confidence score를 인자로 가지는 inference 시각화용 함수 생성. 
+def get_detected_img(model, img_array,  score_threshold=0.3, is_print=True):
+  
+  # 인자로 들어온 image_array를 복사. 
+  draw_img = img_array.copy()
+  bbox_color=(0, 255, 0)
+  text_color=(0, 0, 255)
+
+  # model과 image array를 입력 인자로 inference detection 수행하고 결과를 results로 받음. 
+  # results는 80개의 2차원 array(shape=(오브젝트갯수, 5))를 가지는 list. 
+  results = inference_detector(model, img_array)
+
+  # 80개의 array원소를 가지는 results 리스트를 loop를 돌면서 개별 2차원 array들을 추출하고 이를 기반으로 이미지 시각화 
+  # results 리스트의 위치 index가 바로 COCO 매핑된 Class id. 여기서는 result_ind가 class id
+  # 개별 2차원 array에 오브젝트별 좌표와 class confidence score 값을 가짐. 
+  for result_ind, result in enumerate(results):
+   
+    # 개별 2차원 array의 row size가 0 이면 해당 Class id로 값이 없으므로 다음 loop로 진행. 
+    if len(result) == 0:
+      continue
+    
+    # 2차원 array에서 5번째 컬럼에 해당하는 값이 score threshold이며 이 값이 함수 인자로 들어온 score_threshold 보다 낮은 경우는 제외. 
+    result_filtered = result[np.where(result[:, 4] > score_threshold)]
+    
+    # 해당 클래스 별로 Detect된 여러개의 오브젝트 정보가 2차원 array에 담겨 있으며, 이 2차원 array를 row수만큼 iteration해서 개별 오브젝트의 좌표값 추출. 
+    for i in range(len(result_filtered)):
+    
+      # 좌상단, 우하단 좌표 추출. 
+      left = int(result_filtered[i, 0])
+      top = int(result_filtered[i, 1])
+      right = int(result_filtered[i, 2])
+      bottom = int(result_filtered[i, 3])
+      caption = "{}: {:.4f}".format(labels_to_names_seq[result_ind], result_filtered[i, 4])
+      cv2.rectangle(draw_img, (left, top), (right, bottom), color=bbox_color, thickness=2)
+      cv2.putText(draw_img, caption, (int(left), int(top - 7)), cv2.FONT_HERSHEY_SIMPLEX, 0.6, text_color, 1)
+      if is_print:
+        print(caption)
+
+  return draw_img
+
+################################################################################################################
+### Inference된 여러 image들을 시각화 ###
+################################################################################################################
+
+
+import matplotlib.pyplot as plt
+
+img_arr = cv2.imread('/content/data/images/Abyssinian_88.jpg')
+detected_img = get_detected_img(model, img_arr,  score_threshold=0.3, is_print=True)
+
+# detect 입력된 이미지는 bgr임. 이를 최종 출력시 rgb로 변환 
+detected_img = cv2.cvtColor(detected_img, cv2.COLOR_BGR2RGB)
+
+plt.figure(figsize=(12, 12))
+plt.imshow(detected_img)
+
+
+
+import matplotlib.pyplot as plt
+import cv2
+%matplotlib inline 
+
+def show_detected_images(model, img_arrays, ncols=5):
+    figure, axs = plt.subplots(figsize=(22, 6), nrows=1, ncols=ncols)
+    for i in range(ncols):
+      detected_img = get_detected_img(model, img_arrays[i],  score_threshold=0.5, is_print=True)
+      detected_img = cv2.cvtColor(detected_img, cv2.COLOR_BGR2RGB)
+      #detected_img = cv2.resize(detected_img, (328, 328))
+      axs[i].imshow(detected_img)
+
+        
+show_detected_images(model_ckpt, val_imgs[:5], ncols=5)
+show_detected_images(model_ckpt, val_imgs[5:10], ncols=5)
+
+
+
+val_paths = val_df[val_df['img_path'].str.contains('Persian')]['img_path'].values
+val_imgs = [cv2.imread(x) for x in val_paths]
+
+show_detected_images(model_ckpt, val_imgs[:5], ncols=5)
+show_detected_images(model_ckpt, val_imgs[5:10], ncols=5)
+
+
+
 
